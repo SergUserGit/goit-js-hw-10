10e2609e13111974b36e3a97c254420381d7f71a
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-24 14:56:27 +0200

    debounce settings
    debounce settings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+const debounce = require('lodash.debounce');
+
+const elementSearchBox = document.querySelector('#search-box');
+elementSearchBox.addEventListener(
+  'input',
+  debounce(onSearchBoxInput, DEBOUNCE_DELAY)
+);
+
+function onSearchBoxInput(evt) {
+  console.log(evt.target.value);
+}
