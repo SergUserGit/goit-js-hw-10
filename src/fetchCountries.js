08636b729b6c8fa0631aca6f378cac1cade9afbe
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-24 15:32:52 +0200

    filter
    filter

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,7 @@
 const URL_COUNTRIES = 'https://restcountries.com/v3.1/name/';
 
 export const fetchCountries = name => {
-  return fetch(URL_COUNTRIES + '{' + name + '}');
+  return fetch(URL_COUNTRIES + name).then(response => {
+    return response.json();
+  });
 };
