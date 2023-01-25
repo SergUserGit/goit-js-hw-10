5925bd715441885569761e81a7d74b929d4d19ca
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-25 09:22:32 +0200

    info message
    info message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+
+import Notiflix from 'notiflix';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
+
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'left-top',
+  distance: '70px',
+  opacity: 1,
+});
+
 const DEBOUNCE_DELAY = 300;
 
 const debounce = require('lodash.debounce');
@@ -15,7 +31,11 @@ function onSearchBoxInput(evt) {
   console.log(countriesArray);
   countriesArray
     .then(data => {
-      console.log('Данные', data);
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
     })
     .catch(error => {
       console.log('Ошибка', error);
