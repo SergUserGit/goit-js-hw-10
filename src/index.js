3c90e4851403a84dd64df0a48f2ecabb21edf7dd
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-25 11:36:52 +0200

    new changes
    new changes

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ const DEBOUNCE_DELAY = 300;
 
 const debounce = require('lodash.debounce');
 
+const elCountryList = document.querySelector('.country-list');
+const elCountryInfo = document.querySelector('.country-info');
+
 const elementSearchBox = document.querySelector('#search-box');
 elementSearchBox.addEventListener(
   'input',
@@ -27,17 +30,52 @@ elementSearchBox.addEventListener(
 );
 
 function onSearchBoxInput(evt) {
-  const countriesArray = fetchCountries(evt.target.value);
-  console.log(countriesArray);
+  elCountryList.innerHTML = '';
+  elCountryInfo.innerHTML = '';
+
+  const countriesArray = fetchCountries(evt.target.value.trim());
   countriesArray
     .then(data => {
       if (data.length > 10) {
         Notiflix.Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
+      } else if (data.length >= 2 && data.length <= 10) {
+        const ArrayCountrySort = [...data].sort((a, b) =>
+          a.name.official.localeCompare(b.name.official)
+        );
+
+        const markupArray = ArrayCountrySort.map(el => {
+          return getMarkupListCountries(el.name.official, el.flags.svg);
+        });
+
+        const murkupCountries = markupArray.join('');
+
+        elCountryList.insertAdjacentHTML('afterbegin', murkupCountries);
+      } else if (data.length === 1) {
+        //   console.log(data[0].name.official);
+        //   const strCapitals = data[0].capital.join(',');
+        //   console.log(strCapitals);
+        //   console.log(data[0].population);
+        //   console.log(data[0].flags.svg);
+        console.log(Object.values(data[0].languages).join(','));
       }
     })
     .catch(error => {
       console.log('Ошибка', error);
     });
 }
+
+function getMarkupListCountries(countryName, flagSvgRef) {
+  const strMarkup =
+    '<li class="country-item"><img src="' +
+    flagSvgRef +
+    '" alt="' +
+    countryName +
+    '" width="70" height="40"><p class="contry-name">' +
+    countryName +
+    '</p></li>';
+  return strMarkup;
+}
+
+function getMarkupCountryInfo() {}
